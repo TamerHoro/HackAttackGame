9531001caffcf0c29cb1cdaee23d8096c8bf6423
kuajthjo81894@TechnischeHochschuleN.onmicrosoft.com <kuajthjo81894@TechnischeHochschuleN.onmicrosoft.com>
2021-11-26 13:46:21 +0100

    Renamed Namespace to Game

---
+++ Game/Engine.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace Game
+{
+    
+
+    
+    public partial class Engine : Form
+    {
+        bool goleft, goright, goup, godown, gameover;
+        string facing = "up";
+        //int playerhealth = 100;
+        //int speed = 10;
+       
+
+        
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+            if (gameover) return; // if game over is true then do nothing in this event
+
+            // if the left key is pressed then do the following
+            if (e.KeyCode == Keys.Left)
+            {
+                goleft = true; // change go left to true                                    Goes to PlayerClass
+                facing = "left"; //change facing to left                                    Goes to PlayerClass
+                player.Image = Properties.Resources.playerleft; // change the player image to LEFT image
+            }
+
+            // end of left key selection
+
+            // if the right key is pressed then do the following
+            if (e.KeyCode == Keys.Right)
+            {
+                goright = true; // change go right to true                                   Goes to PlayerClass
+                facing = "right"; // change facing to right                                   Goes to PlayerClass
+                player.Image = Properties.Resources.playeright; // change the player image to right
+            }
+            // end of right key selection
+
+            // if the up key is pressed then do the following
+            if (e.KeyCode == Keys.Up)
+            {
+                facing = "up"; // change facing to up                                   Goes to PlayerClass
+                goup = true; // change go up to true                                   Goes to PlayerClass
+                player.Image = Properties.Resources.playerup; // change the player image to up
+            }
+
+            // end of up key selection
+
+            // if the down key is pressed then do the following
+            if (e.KeyCode == Keys.Down)
+            {
+                facing = "down"; // change facing to down                                   Goes to PlayerClass
+                godown = true; // change go down to true                                   Goes to PlayerClass
+                player.Image = Properties.Resources.playerdown; //change the player image to down
+            }
+        }//Goes to Engine
+
+        private void KeyIsUp(object sender, KeyEventArgs e) // Goes to PlayerClass or Engine
+        {
+            if (e.KeyCode == Keys.Left)     
+            {
+                goleft = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goright = false;
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                goup = false;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                godown = false;
+            }
+
+        }//Goes to Engine
+
+
+
+        public Engine()
+        {
+            Render level = new Render();
+            
+            InitializeComponent();
+            this.Controls.AddRange(level.walling);
+            RestartGame();
+        }
+
+        
+        private void MainTimerEvent(object sender, EventArgs e)
+        {
+            MainTimerEvent MainTimeEvent = new MainTimerEvent();
+            //MainTimeEvent.Update();
+
+
+
+
+        } // Goes to EngineClass
+
+        
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void progressBar1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void player_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Engine_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint_1(object sender, PaintEventArgs e)
+        {
+
+        }      
+        
+        private void ShootBullet(string direction)
+        {
+
+        }
+        
+        private void RestartGame()   //goes to MenueClass
+        {
+            //MakeLevel();
+            //goup = false;
+            //godown = false;
+            //goleft = false;
+            //goright = false;
+            //playerhealth = 100;
+            //GameTimer.Start();
+        }
+    }
+}
---
+++ Game/Render.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Game
+{
+    class Render : Form
+    {
+        int[,] maparray = ReadMapFile();
+        List<PictureBox> levelwalling = new List<PictureBox>();
+        public PictureBox[] walling = new PictureBox[325];
+        List<PictureBox> enemies = new List<PictureBox>();
+        PictureBox goal;       
+        
+        static private int[,] ReadMapFile()
+        {
+            int[,] maparray = new int[18, 18];
+            var lines = File.ReadAllText(@"C:\Users\jonat\source\repos\Test Game (2)\Test Game\Test Game\map array.txt").Split(new string[] { "\n" },
+                                                                                                           StringSplitOptions.None);
+            for (int i = 0; i < 18; i++)
+            {
+                var fields = lines[i].Split(' ');
+                for (int j = 0; j < 18; j++)
+                {
+                    maparray[i, j] = int.Parse(fields[j]);
+                }
+            }
+            return maparray;
+        }
+        public void CreateWalls()            //creates a list with Walls
+        {
+            int l=-10,h=-15,k = 0;
+            for (int i = 0; i < 18; i++)
+            {
+                
+                for (int j = 0; j < 18; j++)
+                {
+                    
+                    if (maparray[i, j] == 1)
+                    {
+                        PictureBox wall = new PictureBox();                        
+                        wall.Tag = $"wall{i}{j}";
+                        wall.Image = Properties.Resources.wall;
+                        wall.Width = 40;
+                        wall.Height = 40;                        
+                        wall.Left = l;
+                        wall.Top = h;
+                        wall.BringToFront();
+                        walling[k++] = wall;
+
+                    }
+                    l = l + 40;
+                }
+                l = -10;
+                h = h + 40;
+
+            }
+           
+        }   
+        //public PictureBox LoadMap()
+        //{
+        //    PictureBox[] walling = new PictureBox[levelwalling.];
+        //    foreach(PictureBox p in levelwalling)
+        //    {
+        //        return p;
+        //    }
+        //    return null;
+        //}
+
+        public Render()
+        {
+            CreateWalls();
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Render
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "Render";
+            this.Load += new System.EventHandler(this.Render_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        private void Render_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
