a1fedf625a06d3b5b3b9bf79e852ec35b0a375c4
molitorjo64059 <molitorjo64059@TechnischeHochschuleN.onmicrosoft.com>
2021-12-23 14:33:11 +0100

    Destructable Walls and ability to destroy them with bullets added

---
+++ Game/GameObjects/DestructableWall.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    class DestructableWall : Wall
+    {
+
+        public DestructableWall(int l, int h, int i, int j) : base(l, h, i, j)
+        {
+            this.Tag = $"destructablewall{i}{j}";
+            this.Image = Properties.Resources.walldest;
+            this.Width = 40;
+            this.Height = 40;
+            this.Left = l;
+            this.Top = h;
+            this.BringToFront();
+        }
+    }
+}
